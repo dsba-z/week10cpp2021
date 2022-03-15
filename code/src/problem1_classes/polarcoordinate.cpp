efe1fb6920b6c3dd2ee818672b61168cb816be69
Georgii Zhulikov <l8doku@gmail.com>
2022-03-15 12:20:06 +0300

    WIP 18

--- code/src/problem1_classes/polarcoordinate.cpp
+++ code/src/problem1_classes/polarcoordinate.cpp
@@ -1,6 +1,53 @@
 #include "polarcoordinate.h"
+#include <utility>
 
-PolarCoordinate::PolarCoordinate()
+PolarCoordinate::PolarCoordinate(double r, double a)
 {
+    radius = r;
+    angle = a;
+}
+
+
+PolarCoordinate PolarCoordinate::operator+(const PolarCoordinate& b) const
+{
+    radius;
+    angle;
+    
+    double x;
+    double y;
+    
+    
+    // option 1
+    toCartesian(x, y); // by reference
+    
+    
+    // option 2
+    x = getX();
+    y = getY();
+    
+    // option 3
+    std::pair<double, double> cartesian = toCartesianPair();
+    
+    
+    b.radius;
+    b.angle;
+    
+    double bx = b.getX();
+    double by = b.getY();
+    
+    
+    double resultX = x + bx;
+    double resultY = y + by;
+    
+//    PolarCoordinate resultCreation(radius, angle); // inside cartesianToPolar
+    PolarCoordinate result = cartesianToPolar(resultX, resultY);
+    
+    return result;
     
 }
+
+PolarCoordinate& PolarCoordinate::operator+=(const PolarCoordinate& b)
+{
+    *this = *this + b;
+}
+
