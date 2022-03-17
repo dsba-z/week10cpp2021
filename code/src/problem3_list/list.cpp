88b51fd8988dab714a09fe35fa21380f802b2cc8
Georgii Zhulikov <l8doku@gmail.com>
2022-03-17 15:17:23 +0300

    Exception example

--- code/src/problem3_list/list.cpp
+++ code/src/problem3_list/list.cpp
@@ -1,6 +1,14 @@
 #include "list.h"
+#include <vector>
+#include <stdexcept>
 
-List::List()
+List::List(const std::vector<int>& v)
 {
     
+    // copy elements
+    
+    if (v.size() > List::MAX_CAPACITY)
+    {
+        throw std::range_error("Vector has too many elements");
+    }
 }
--- code/src/problem3_list/main.cpp
+++ code/src/problem3_list/main.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <sstream>
 #include <fstream>
+#include <stdexcept>
 
 #include "list.h"
 
@@ -9,6 +10,11 @@
 int main()
 {
 
-    
+    try {
+        List({1,4,5,5,5,6,6,6,6,5,6,6,6,5,6,6,6,5});
+        
+    }  catch (const std::range_error& e) {
+        std::cout << e.what() << std::endl;
+    }
     return 0;
 }
